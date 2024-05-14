3cde1856e6c178012e3fc5dfb9731c10fbce68e5
MessiasFilho <messiasfilho98@gmail.com>
2024-05-14 18:22:16 -0300

    updated schema prisma

---
+++ prisma/migrations/20240514211859_updated_db/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `fone` VARCHAR(20) NULL,
+    `cnpj` VARCHAR(255) NULL,
+    `cpf` VARCHAR(14) NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `confpassword` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updated_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
